3ba81a53fc3bd871392f2ba10870000ab0fb393f
Fabio Santos <fabioprogramadorti@gmail.com>
2018-09-01 23:09:20 -0300

    ...

--- view/usuario.php
+++ view/usuario.php
@@ -11,30 +11,58 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Sistema Base</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../style/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="../style/css/signin.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../style/css/style-projeto.css" rel="stylesheet">
+    
+    <!--Jquery script para efeito de Toggle-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        $("#acomp1").click(function(){
+            $('#input_one').hide();            
+        });
+        $("#acomp2").click(function(){
+            $('#input_one').hide();
+            $('#input_two').hide(); 
+        });
+        $("#acomp3").click(function(){
+            $('#input_one').hide();
+            $('#input_two').hide(); 
+            $('#input_tree').hide();
+        });
+    });
+    </script>
   </head>
 
-  <body class="text-center">
-    <form class="form-signin">
-      <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
-  </body>
+    <body class="text-center">
+        <form class="form-signin">
+            <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Incluir Usuário</h1>      
+            <input type="" id="" class="form-control" placeholder="Usuário" autofocus>
+            <div class="checkbox mb-3"></div>
+            <div class="checkbox mb-3">
+                Acompanhantes
+            </div>
+            <button id="acomp1" class="btn btn-primary button_change">1</button>
+            <button id="acomp2" class="btn btn-primary button_change">2</button>
+            <button id="acomp3" class="btn btn-primary button_change">3</button>
+            <div class="checkbox mb-3"></div>
+            
+            <input id="input_one" class="form-control" placeholder="Acompanhante 1" >
+            
+            <input id="input_two" class="form-control" placeholder="Acompanhante 2" >
+            
+            <input  id="input_tree" class="form-control" placeholder="Acompanhante 3" >            
+            <div class="checkbox mb-3"></div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Enviar</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+        </form>
+    </body>
 </html>
